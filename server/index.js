4916d0ba88601bd9db7b160336882f733f8c0c51
idivyanshsn <divyansh.23bce10887@vitbhopal.ac.in>
2026-08-21 17:58:48 +0530

    Prepare frontend and backend for deployment

--- server/index.js
+++ server/index.js
@@ -11,9 +11,10 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: process.env.CLIENT_URL,
+    credentials: true
 }))
+
 
 app.use(express.json())
 app.use(cookieParser())
